8c6670d6539e071c01aa6f0c667b2446f4e37e14
Angela <antunezangela89@gmail.com>
2024-01-01 12:48:31 +0100

    Create de pokemon cards by retrieving the data from the API

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const poke_container = document.getElementById('poke-container')
-const pokemon_count = 150
+const poke_container = document.getElementById('poke-container');
+const pokemon_count = 150;
 const colors = {
     fire: '#FDDFDF',
     grass: '#DEFDE0',
@@ -15,4 +15,42 @@ const colors = {
 	flying: '#F5F5F5',
 	fighting: '#E6E0D4',
 	normal: '#F5F5F5'
+};
+
+const myEndpoint = 'https://pokeapi.co/api/v2/pokemon/';
+
+for (let id = 1; id <= pokemon_count; id++) {
+	await createPokemonCard(id);
+}
+
+async function createPokemonCard(id) {
+	const pokemonEndpoint = `${myEndpoint}${id}`;
+
+	// declaro una constante y le asigno el response (await fetch)
+	try {
+		const response = await fetch(pokemonEndpoint);
+
+	// obtengo el json de ese response y lo asigno a otra constante
+		const data = await response.json();
+
+	// creo el html que voy a insertar, con template, lo inserto en poke-container con insertAdjacentHTML
+		poke_container.insertAdjacentHTML('beforeend',
+			`<article class="pokemon" style="background-color: rgb(222,253,224);">
+				<div class="img-container">
+					<img src=${data.sprites.other["official-artwork"].front_default} alt=${data.name}>
+				</div>
+				<div class="info">
+					<span class="number">00${data.id}</span>
+					<h3 class="name">${data.name}</h3>
+					<small class="type">Type: <span>${data.types[0].name}</span></small>
+				</div>
+			</article>`)
+	// falta usar el método padStart para strings
+	// falta cambiar el background en función del tipo de pokemon 
+
+	} catch (error) {
+		console.log(error);
+	}
+
+	console.log(pokemonEndpoint);
 }
