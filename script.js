ad03d45702ef56e481b5c4b042b7887c7a4d7b27
Angela <antunezangela89@gmail.com>
2024-01-01 16:28:13 +0100

    Delete comments

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
-//cojo el contenedor en el que voy a insertar cada card
 const poke_container = document.getElementById('poke-container');
-//guardo una constante para el recuento de pokemons
+
 const pokemon_count = 150;
-//almaceno en un objeto con clave-valor el color que se corresponde con cada tipo de pokemon
+
 const colors = {
     fire: '#FDDFDF',
     grass: '#DEFDE0',
@@ -20,39 +19,29 @@ const colors = {
 	normal: '#F5F5F5'
 };
 
-//almaceno el endpoint de todos los pokemons
 const myEndpoint = 'https://pokeapi.co/api/v2/pokemon/';
 
-//creo un contador para crear las 150 cards
 for (let id = 1; id <= pokemon_count; id++) {
 	await createPokemonCard(id);
 }
 
-//creo la función asíncrona de crear cada card
 async function createPokemonCard(id) {
-	// añado el id del pokemon
+
 	const pokemonEndpoint = `${myEndpoint}${id}`;
 	
 	try {
-		// declaro una constante y le asigno el response (await fetch)
 		const response = await fetch(pokemonEndpoint);
 		
-		// obtengo el json de ese response y lo asigno a otra constante
 		const pokeData = await response.json();
 
-		//convierto el id en string
 		const idAsString = pokeData.id.toString();
 
-		// añado el padding de 0 al principio
 		const paddedId = idAsString.padStart(3, '0');
 
-		// obtengo el type de cada pokemon
 		const type = pokeData.types[0].type.name;
 
-		//obtengo la img de cada pokemon
 		const img = pokeData.sprites.other["official-artwork"].front_default;
 
-	// creo el html que voy a insertar, con template, lo inserto en poke-container con insertAdjacentHTML
 		poke_container.insertAdjacentHTML('beforeend',
 			`<article class="pokemon" style="background-color: ${colors[type]}">
 				<div class="img-container">
